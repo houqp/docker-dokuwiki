479fb9f636878f05aca93edfad1991beee984744
Qingping Hou <dave2008713@gmail.com>
2015-08-20 01:36:26 -0700

    restore with version

--- restore.sh
+++ restore.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker create -p 8000:80 --name dokuwiki houqp/dokuwiki
+docker create -p 8000:80 --name dokuwiki houqp/dokuwiki:2015-08-10
 docker run --volumes-from dokuwiki --name dokuwiki-data busybox
 docker run --rm --volumes-from dokuwiki -w / -v $(pwd):/backup ubuntu tar xzvf /backup/dokuwiki-backup.tar.gz
 docker start dokuwiki
